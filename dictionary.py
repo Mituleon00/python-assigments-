c6b198bb3a8d775b6aa389fa56f4eb9dc87cea9c
Mituleon00 <mituleon15@gmail.com>
2026-02-23 11:15:29 +0300

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+student= {
+    "name":"Leon",
+    "reg_no":"BSCIT-05-0082/2024",
+    "DOB":"2005"
+    }
+print(student)
+print(student.keys())
+print(student.values())
+print(student.items())
+student.update({"email":"Leonmitu15@gmail.com"})
+print(student)
+student.get("name")
